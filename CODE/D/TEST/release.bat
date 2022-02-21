d165ccca5ad6955fe7d5a13d9b93d4dc2e9f5217
senselogic <dev.senselogic@gmail.com>
2022-02-21 20:33:55 +0100

    % release

--- CODE/D/TEST/release.bat
+++ CODE/D/TEST/release.bat
@@ -1,5 +1,5 @@
 del /q test.exe
-dmd -O -inline -m64 -oftest.exe test.d ../base.d ../buffer.d ../cell.d ../cell_position_vector.d ../cloud.d ../component.d ../compression.d ../image.d ../property.d ../scalar.d ../scan.d ../stream.d ../vector_3.d ../vector_4.d
+dmd -O -m64 -oftest.exe test.d ../base.d ../buffer.d ../cell.d ../cell_position_vector.d ../cloud.d ../component.d ../compression.d ../image.d ../property.d ../scalar.d ../scan.d ../stream.d ../vector_3.d ../vector_4.d
 del /q *.obj
 del /q *.pdb
 pause
